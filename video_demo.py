ab49126e89a478158fd6cbce6bb945f0225715f7
akshayrajeev1996 <akshayrajeev1996@gmail.com>
2019-12-08 12:30:40 -0500

    Update video_demo.py

--- video_demo.py
+++ video_demo.py
@@ -1,31 +1,32 @@
 from __future__ import division
 import time
-import torch 
+import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
-import cv2 
+import cv2
 from util import *
 from darknet import Darknet
 from preprocess import prep_image, inp_to_image, letterbox_image
 import pandas as pd
-import random 
+import random
 import pickle as pkl
 import argparse
 
 
 def get_test_input(input_dim, CUDA):
     img = cv2.imread("dog-cycle-car.png")
-    img = cv2.resize(img, (input_dim, input_dim)) 
-    img_ =  img[:,:,::-1].transpose((2,0,1))
-    img_ = img_[np.newaxis,:,:,:]/255.0
+    img = cv2.resize(img, (input_dim, input_dim))
+    img_ = img[:, :, ::-1].transpose((2, 0, 1))
+    img_ = img_[np.newaxis, :, :, :] / 255.0
     img_ = torch.from_numpy(img_).float()
     img_ = Variable(img_)
-    
+
     if CUDA:
         img_ = img_.cuda()
-    
+
     return img_
+
 
 def prep_image(img, inp_dim):
     """
@@ -37,9 +38,10 @@ def prep_image(img, inp_dim):
     orig_im = img
     dim = orig_im.shape[1], orig_im.shape[0]
     img = (letterbox_image(orig_im, (inp_dim, inp_dim)))
-    img_ = img[:,:,::-1].transpose((2,0,1)).copy()
+    img_ = img[:, :, ::-1].transpose((2, 0, 1)).copy()
     img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
     return img_, orig_im, dim
+
 
 def write(x, img):
     c1 = tuple(x[1:3].int())
@@ -47,40 +49,41 @@ def write(x, img):
     cls = int(x[-1])
     label = "{0}".format(classes[cls])
     color = random.choice(colors)
-    cv2.rectangle(img, c1, c2,color, 1)
-    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+    cv2.rectangle(img, c1, c2, color, 1)
+    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
     c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
-    cv2.rectangle(img, c1, c2,color, -1)
-    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
+    cv2.rectangle(img, c1, c2, color, -1)
+    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225, 255, 255], 1);
     return img
+
 
 def arg_parse():
     """
     Parse arguements to the detect module
     
     """
-    
-    
+
     parser = argparse.ArgumentParser(description='YOLO v3 Video Detection Module')
-   
-    parser.add_argument("--video", dest = 'video', help = 
-                        "Video to run detection upon",
-                        default = "video.avi", type = str)
-    parser.add_argument("--dont_show", dest = 'dont_show', help = 
-                        "To show output.",
-                        default = 0, type = str)
-    parser.add_argument("--dataset", dest = "dataset", help = "Dataset on which the network has been trained", default = "pascal")
-    parser.add_argument("--confidence", dest = "confidence", help = "Object Confidence to filter predictions", default = 0.5)
-    parser.add_argument("--nms_thresh", dest = "nms_thresh", help = "NMS Threshhold", default = 0.4)
-    parser.add_argument("--cfg", dest = 'cfgfile', help = 
-                        "Config file",
-                        default = "cfg/yolov3.cfg", type = str)
-    parser.add_argument("--weights", dest = 'weightsfile', help = 
-                        "weightsfile",
-                        default = "yolov3.weights", type = str)
-    parser.add_argument("--reso", dest = 'reso', help = 
-                        "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
-                        default = "416", type = str)
+
+    parser.add_argument("--video", dest='video', help=
+    "Video to run detection upon",
+                        default="video.avi", type=str)
+    parser.add_argument("--dont_show", dest='dont_show', help=
+    "To show output.",
+                        default=0, type=str)
+    parser.add_argument("--dataset", dest="dataset", help="Dataset on which the network has been trained",
+                        default="pascal")
+    parser.add_argument("--confidence", dest="confidence", help="Object Confidence to filter predictions", default=0.5)
+    parser.add_argument("--nms_thresh", dest="nms_thresh", help="NMS Threshhold", default=0.4)
+    parser.add_argument("--cfg", dest='cfgfile', help=
+    "Config file",
+                        default="cfg/yolov3.cfg", type=str)
+    parser.add_argument("--weights", dest='weightsfile', help=
+    "weightsfile",
+                        default="yolov3.weights", type=str)
+    parser.add_argument("--reso", dest='reso', help=
+    "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
+                        default="416", type=str)
     return parser.parse_args()
 
 
@@ -88,7 +91,7 @@ if __name__ == '__main__':
     args = arg_parse()
     confidence = float(args.confidence)
     nms_thesh = float(args.nms_thresh)
-    dont_show=bool(args.dont_show)
+    dont_show = bool(args.dont_show)
     start = 0
 
     CUDA = torch.cuda.is_available()
@@ -96,9 +99,9 @@ if __name__ == '__main__':
     num_classes = 80
 
     CUDA = torch.cuda.is_available()
-    
+
     bbox_attrs = 5 + num_classes
-    
+
     print("Loading network.....")
     model = Darknet(args.cfgfile)
     model.load_weights(args.weightsfile)
@@ -106,47 +109,53 @@ if __name__ == '__main__':
 
     model.net_info["height"] = args.reso
     inp_dim = int(model.net_info["height"])
-    assert inp_dim % 32 == 0 
+    assert inp_dim % 32 == 0
     assert inp_dim > 32
 
     if CUDA:
         model.cuda()
-        
+
     model(get_test_input(inp_dim, CUDA), CUDA)
 
     model.eval()
-    
+
     videofile = args.video
-    
+
     cap = cv2.VideoCapture(videofile)
-    
+
     assert cap.isOpened(), 'Cannot capture source'
-    
+
     frames = 0
     start = time.time()
-    out = cv2.VideoWriter('output.mp4', 0x7634706d, 20.0, (640,480))
+    #fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    #out = cv2.VideoWriter('output.mp4', 0x7634706d, 20.0, (640, 480))
+    FRAME_WIDTH = cap.get(3)
+    FRAME_HEIGHT = cap.get(4)
+    FRAME_FPS = cap.get(5)
+    FRAME_FOURCC = cap.get(6)
+
+    output_file = 'output.avi'
+    out = cv2.VideoWriter(output_file, int(FRAME_FOURCC), FRAME_FPS, (int(FRAME_WIDTH), int(FRAME_HEIGHT)))
     while cap.isOpened():
-        
+
         ret, frame = cap.read()
         if ret:
-            
 
             img, orig_im, dim = prep_image(frame, inp_dim)
-            
-            im_dim = torch.FloatTensor(dim).repeat(1,2)                        
-            
-            
+
+            im_dim = torch.FloatTensor(dim).repeat(1, 2)
+
             if CUDA:
                 im_dim = im_dim.cuda()
                 img = img.cuda()
-            
-            with torch.no_grad():   
+
+            with torch.no_grad():
                 output = model(Variable(img), CUDA)
-            output = write_results(output, confidence, num_classes, nms = True, nms_conf = nms_thesh)
+            output = write_results(output, confidence, num_classes, nms=True, nms_conf=nms_thesh)
 
             if type(output) == int:
                 frames += 1
-                print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
+                print("FPS of the video is {:5.2f}".format(frames / (time.time() - start)))
                 out.write(orig_im)
                 if not dont_show:
                     cv2.imshow("frame", orig_im)
@@ -154,27 +163,23 @@ if __name__ == '__main__':
                     if key & 0xFF == ord('q'):
                         break
                 continue
-            
-            
 
-            
             im_dim = im_dim.repeat(output.size(0), 1)
-            scaling_factor = torch.min(inp_dim/im_dim,1)[0].view(-1,1)
-            
-            output[:,[1,3]] -= (inp_dim - scaling_factor*im_dim[:,0].view(-1,1))/2
-            output[:,[2,4]] -= (inp_dim - scaling_factor*im_dim[:,1].view(-1,1))/2
-            
-            output[:,1:5] /= scaling_factor
-    
+            scaling_factor = torch.min(inp_dim / im_dim, 1)[0].view(-1, 1)
+
+            output[:, [1, 3]] -= (inp_dim - scaling_factor * im_dim[:, 0].view(-1, 1)) / 2
+            output[:, [2, 4]] -= (inp_dim - scaling_factor * im_dim[:, 1].view(-1, 1)) / 2
+
+            output[:, 1:5] /= scaling_factor
+
             for i in range(output.shape[0]):
-                output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, im_dim[i,0])
-                output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, im_dim[i,1])
-            
+                output[i, [1, 3]] = torch.clamp(output[i, [1, 3]], 0.0, im_dim[i, 0])
+                output[i, [2, 4]] = torch.clamp(output[i, [2, 4]], 0.0, im_dim[i, 1])
+
             classes = load_classes('data/coco.names')
             colors = pkl.load(open("pallete", "rb"))
-            
+
             list(map(lambda x: write(x, orig_im), output))
-           
             out.write(orig_im)
             if not dont_show:
                 cv2.imshow("frame", orig_im)
@@ -182,16 +187,8 @@ if __name__ == '__main__':
                 if key & 0xFF == ord('q'):
                     break
             frames += 1
-            print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
+            print("FPS of the video is {:5.2f}".format(frames / (time.time() - start)))
 
-            
+
         else:
             break
-    
-
-
-    
-
-    
-    
-
